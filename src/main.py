403db93a29df6a03453f290ed3d4c3f7de274d02
MateuszBodek <maitkbod@gmail.com>
2025-01-08 13:03:26 +0100

    added lights running script, upgraded visual view, more data collecting

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src.models.queue import Queue
 from src.models.lights_system import LightsSystem
 from src.models.traffic_lights import TrafficLightState
+from typing import List
 
 left_light_system: LightsSystem = LightsSystem.create(TrafficLightState.GREEN, 100)
 left_light_system.generate_lights_timestamps()
@@ -17,12 +18,49 @@ right_queue: Queue = Queue.create_queue(
     right_light_system.drivers, right_light_system.lights_timestamps
 )
 
-happy_drivers = []
-for _ in range(10):
-    print(left_queue.light_state)
-    left_queue.run()
-    new_happy_drivers = [i for i in left_queue.happy_drivers if i not in happy_drivers]
-    happy_drivers.extend(new_happy_drivers)
-    for i in new_happy_drivers:
-        print(i.black_box, i.name, i.driver_id)
+
+def print_que_state(queue: Queue, cosh: List):
     print()
+    for driver in queue.happy_drivers:
+        if driver not in cosh:
+            print(f'{driver.black_box, str(driver.name), driver.driver_id}')
+            cosh.append(driver)
+    print()
+
+
+def print_red_queue(queue: Queue):
+    print()
+    for driver in queue.cars:
+        print(f'WAITING: {driver.black_box, str(driver.name), driver.driver_id}')
+
+
+cosh1 = []
+cosh2 = []
+space = ' '
+
+
+def run_cycle(queue1: Queue, queue2: Queue):
+    if queue1.light_state:
+        print('Green' if queue2.light_state else 'Red')
+        queue2.run()
+        print_red_queue(queue2)
+        print('  -  '*30)
+        print('Green' if queue1.light_state else 'Red')
+        queue1.run()
+        print_que_state(queue1, cosh1)
+    else:
+        l1_temp = queue1.light_state
+        queue1.run()
+
+        print('Green' if queue2.light_state else 'Red')
+        queue2.run()
+        print_que_state(queue2, cosh2)
+        print('  -  '*30)
+        print('Green' if l1_temp else 'Red')
+        print_red_queue(queue1)
+
+
+for cycle in range(20):
+    print('-' * 150)
+    print(f'\n{space*75}CYCLE {cycle}\n')
+    run_cycle(left_queue, right_queue)
