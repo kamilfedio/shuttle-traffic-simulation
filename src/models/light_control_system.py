08084b1f269e8646e532d1764da2a512164d4c65
MateuszBodek <maitkbod@gmail.com>
2025-01-09 15:52:31 +0100

    control system tests added

--- src/models/light_control_system.py
+++ src/models/light_control_system.py
@@ -1,6 +1,6 @@
 '''
 Co to ma robic?
-- przyjmuje bieżący stan świateł (które teraz będzie zielone i ile osób w kolejce)
+- przyjmuje bieżący stan świateł (które teraz będzie zielone i ile osób w kolejce) queue1 jest zielone
 - oblicza różnicę długości kolejek i wstawia ją jako x do funkcji a * x = y, gdzie y to mnożnik bazowego czasu zielonego światła
   (jego trzeba zoptymalizować najpierw, żebyśmy mieli optymalny baseline dla x = 0)
 - trzeba jakoś znaleźć optymalną wartość a, może brute force wypróbować różne wartości np. od 0 do 10 i zobaczyć jakie dają wyniki
@@ -12,20 +12,20 @@ class ControlSystem:
     def __init__(
             self,
             basetime: float | int,
-            alpha: float
+            alpha: float | None
     ):
         self.basetime: float | int = basetime
         self.alpha: float | None = alpha
 
     @classmethod
-    def create_control_system(cls, basetime: float | int, *args) -> "ControlSystem":
-        alpha = args[0] if args else None
+    def create_control_system(cls, basetime: float | int, alpha: float | None = None) -> "ControlSystem":
         return cls(basetime, alpha)
 
     def calculate_time(self, queue1: int, queue2: int) -> tuple[float, ...]:
         difference: int = queue1 - queue2
-        print(self.alpha)
+        print(type(self.alpha))
         if self.alpha:
             green_time: float = (self.alpha ** difference) * self.basetime
+            print(self.alpha, difference, self.basetime)
             return tuple([green_time, green_time + 5])
         return tuple([self.basetime, self.basetime + 5])
---
+++ test/test_light_control_system.py
@@ -0,0 +1,35 @@
+from src.models.light_control_system import ControlSystem
+'''
+- przy tworzeniu ładuje basetiome zielonego (w nowej wersji będzie to tylko do 1 iteracji - gdy nie ma alpha.
+  Najlepiej gdyby w pętli: generowanie tupli czasów, queue.run(), pobieranie danych kolejkowych do next loopa
+- przyjmuje queue1 i queue2, q1 będzie teraz zielona
+- wypluwa tuple(zielone, zielone + 5)
+'''
+
+
+def _create_light_control_system(basetime: float | int, alpha: int| float = None):
+    return ControlSystem.create_control_system(basetime, alpha)
+
+
+def test_create_light_control_system():
+    control_system: ControlSystem = _create_light_control_system(15)
+    assert control_system is not None
+    isinstance(control_system, ControlSystem)
+    assert control_system.basetime == 15
+    assert not control_system.alpha
+
+
+def test_calculate_time():
+    control_system: ControlSystem = _create_light_control_system(15)
+    assert control_system.calculate_time(0, 0) == (15, 20)
+    assert control_system.calculate_time(10, 10) == (15, 20)
+    assert control_system.calculate_time(0, 2) == (15, 20)
+    assert control_system.calculate_time(-4, 0) == (15, 20)
+    assert control_system.calculate_time(1000, 0) == (15, 20)
+
+def test_calculate_time_alpha():
+    control_system: ControlSystem = _create_light_control_system(15, 2)
+    assert control_system.calculate_time(1, 0) == (30, 35)
+    assert control_system.calculate_time(1, 0)[::-1] == (35, 30)
+    assert control_system.calculate_time(1, 1) == (15, 20)
+    assert control_system.calculate_time(0, 1) == (7.5, 12.5)
