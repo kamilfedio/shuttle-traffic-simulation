3c07ec8cedda4f898c771a6277136491b46255be
Kamil Fedio <kamilf827@gmail.com>
2025-01-17 15:12:14 +0100

    simulation summary

--- src/utils/printing_methods.py
+++ src/utils/printing_methods.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any
 from src.models.queue import Queue
 
 
@@ -48,3 +48,17 @@ def print_queue_state(queue: Queue, cosh: List, is_right: bool):
     print('-' * 112)
 
     print()
+
+
+def simulation_summary(blackbox: dict[str, Any]) -> None:
+    print(f'\n\n{"S T A T S":^158}')
+    print("-" * 158)
+    print(f'| DIRECTION | FIRST COLOR | DRIVERS SERVED | NO-WAIT DRIVERS | AVG WAIT TIME | AVG GREEN WAIT TIME | '
+          f'AVG DRIVERS BEHIND | AVG QUEUE LENGTH | AVG CYCLE WAITS |')
+    print('-' * 158)
+    for key, blackbox in blackbox:
+        print(
+            f"| {blackbox['direction']:<9} | {blackbox['first_color']:<11} | {blackbox['avgs']['drivers_served']:<14} | {blackbox['avgs']['no_stop_drivers']:<15} | {blackbox['avgs']['avg_waiting_times']:<13.2f} | "
+            f"{blackbox['avgs']['avg_green_waiting_times']:<19.2f} | {blackbox['avgs']['avg_queue_length']:<18.2f} | "
+            f"{blackbox['avg_queue_length']:<16.2f} | {blackbox['avg_cycle_wait']:<15.2f} |")
+        print('-' * 158)
