8afdb379bf5cf3b800ca33d0f8f35514208669ed
Kamil Fedio <kamilf827@gmail.com>
2025-01-29 20:09:26 +0100

    convert data

--- src/models/model_comparison.py
+++ src/models/model_comparison.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+import numpy as np
+
 from src.models.simulation import Simulation
 from tqdm import tqdm
 import pandas as pd
@@ -33,7 +37,7 @@ class Evaluation:
         return self.data_control_system
 
 
-cases = [('low', 'low'), ('low', 'mid')]# ('low', 'high'), ('mid', 'mid'), ('mid', 'high'), ('high', 'high')]
+cases = [('low', 'low'), ('low', 'mid')]# ('low', 'mid') ('low', 'high'), ('mid', 'mid'), ('mid', 'high'), ('high', 'high')]
 results_normal = []
 results_control_system = []
 
@@ -43,7 +47,43 @@ for left, right in cases:
     results_normal.append(evaluation.get_results_normal)
     results_control_system.append(evaluation.get_results_control_system)
 
-print(results_normal)
-print()
-df_norm = pd.DataFrame.from_dict(results_normal[0])
+data_values: list[dict[str, Any]] = []
+
+for case in results_normal:
+    cases_values = {}
+    for tries in case:
+        for queue, data in tries.items():
+            for key, value in data.items():
+                if key == 'avgs':
+                    for k, avg in value.items():
+                        keyname: str = f'{queue}_{key}_{k}'
+
+                        if not cases_values.get(keyname):
+                            cases_values[keyname] = []
+
+                        cases_values[keyname].append(avg)
+
+                else:
+                    keyname: str = f'{queue}_{key}'
+
+                    if not cases_values.get(keyname):
+                        cases_values[keyname] = []
+
+                    cases_values[keyname].append(value)
+
+    data_values.append(cases_values)
+
+for dicti in data_values:
+    for k, v in dicti.items():
+        if 'queue_avgs_no_stop_drivers' in k:
+            tmp = [float(i[:-1]) for i in v]
+            dicti[k] = np.mean(tmp)
+        elif 'avgs' in k:
+            dicti[k] = np.mean(v)
+        else:
+            dicti[k] = v[0]
+
+df_norm = pd.DataFrame.from_records(data_values)
 print(df_norm)
+
+print([col for col in df_norm.columns])
