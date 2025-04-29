ed55ec70aede1cfef2fb14ca4cd388d4ff942c2a
Mateusz Bodek <matikbod@gmail.com>
2025-04-29 20:47:13 +0200

    minor changes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.models.simulation import Simulation
 
 
 
-simulation = Simulation.create(is_debugging=True, left_intensity='high', right_intensity='high', use_control_system=False)
+simulation = Simulation.create(is_debugging=True, left_intensity='mid', right_intensity='mid', use_control_system=False)
 simulation.simulate()
 
 print(simulation.get_avg_wait_time())
